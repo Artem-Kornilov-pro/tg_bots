5ad3921aa1fe66d5620d5e308886f8773b67d120
Артемий <artemijkornilov2@gmail.com>
2025-01-11 18:14:35 +0300

    Update app_5.py
    Добавил команду для удаления

--- app_5.py
+++ app_5.py
@@ -1,4 +1,3 @@
-#список задача
 import telebot
 
 API_TOKEN = ""
@@ -7,7 +6,7 @@ tasks = []
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.reply_to(message, "Привет! Напиши /add задача, чтобы добавить задачу, или /list, чтобы увидеть список.")
+    bot.reply_to(message, "Привет! Напиши /add задача, чтобы добавить задачу, /list, чтобы увидеть список, или /delete номер, чтобы удалить задачу.")
 
 @bot.message_handler(commands=['add'])
 def add_task(message):
@@ -21,8 +20,22 @@ def add_task(message):
 @bot.message_handler(commands=['list'])
 def list_tasks(message):
     if tasks:
-        bot.reply_to(message, "Список задач:\n" + "\n".join(f"- {t}" for t in tasks))
+        bot.reply_to(message, "Список задач:\n" + "\n".join(f"{i+1}. {t}" for i, t in enumerate(tasks)))
     else:
         bot.reply_to(message, "Список задач пуст.")
+
+@bot.message_handler(commands=['delete'])
+def delete_task(message):
+    try:
+        task_number = int(message.text[8:])  # Получаем номер задачи после команды
+        if 1 <= task_number <= len(tasks):
+            deleted_task = tasks.pop(task_number - 1)  # Удаляем задачу по номеру
+            bot.reply_to(message, f"Задача '{deleted_task}' удалена!")
+        else:
+            bot.reply_to(message, "Неверный номер задачи.")
+    except ValueError:
+        bot.reply_to(message, "Укажите номер задачи после команды /delete.")
+    except IndexError:
+        bot.reply_to(message, "Задача с таким номером не существует.")
 
 bot.polling()
